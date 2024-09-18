29dc62f4249332aefef80adfdc1844c7544bb3ba
sawlnanda <ferstival1401@gmail.com>
2024-09-18 09:53:30 -0300

    Add files via upload

---
+++ AULA01_JS_NANDA_ENRIQUE/1-meuPrimeiroPrograma.js
@@ -0,0 +1,14 @@
+//alert('testando o script')
+var nome = prompt('Qual seu nome?')
+
+var sobrenome = prompt('Qual seu sobrenome')
+
+console.log("Ola " + nome + " " + sobrenome)
+
+//saida do DOM
+
+document.write("<h1> Bem vindo " +nome+" "+sobrenome+"</h1>")
+
+document.write("Steve atraves do JavaScript")
+
+document.write('<img src=https://sujeitoprogramador.com/steve.png alt=foto do steve>')
